89f15f6d9c327442389d95069c4936a0661409ff
javi <javichu25.7.96@gmail.com>
2020-05-13 00:45:28 +0200

    setup update

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.5',
     description='Analisis de la potencia generada en un cubesat donde la superficie es limitada, calculando la sombra que se puede producir, incluyendo la posicion del satelite',
+    url = 'https://github.com/j1996/EPM_UCC',
     author='Javier Martinez Martinez @Altran',
+    author_email='javi.mtnez.mtnez@gmail.com',
     license='MIT',
+    install_requires=['trimesh','poliastro','astropy'],
 )
